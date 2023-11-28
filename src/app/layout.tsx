b56b084c91534b7e9114d76c610f1f6ae34cada8
Majid-pkz <majid.computing@gmail.com>
2023-11-29 00:52:30 +1100

    import Poppins Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 import './globals.css'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
+import { Poppins } from 'next/font/google'
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["900", "800", "700", "600", "500", "100"],
+  style: ["normal"],
+});
 
 export const metadata: Metadata = {
   title: 'Neurodiversity Academy',
@@ -16,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>{children}</body>
     </html>
   )
 }
