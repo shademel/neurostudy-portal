2e997a9d41b4753eefd301eb2c06de0f67df8a77
Pratik Bhumkar <pratik@neurodiverisityacademy.com>
2024-01-05 22:55:14 +1100

    Adding GTag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,30 @@ import ButtonDisplay from './components/buttons/ButtonDisplay';
 import Example from './components/typography/Example';
 import BadgeDisplay from './components/badges/BadgeDisplay';
 import CardList from './components/article/card';
+import Script from 'next/script';
+
+const getGoogleAnalyticsScript = () => {
+  return (
+    <>
+      <Script src='https://www.googletagmanager.com/gtag/js?id=G-5YMLVTTK45' />
+      <Script id='google-analytics'>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-5YMLVTTK45');
+        `}
+      </Script>
+    </>
+  );
+};
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <BadgeDisplay />
-      <div className={styles.description}>
-        <h1>Site under construction</h1>
-      </div>
-
+      {getGoogleAnalyticsScript()}
       <div>
         <ButtonDisplay />
         <Example />
