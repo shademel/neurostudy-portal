aa871f60b81d17e6fab7ae5bfc30801363d4d266
Majid-pkz <majid.computing@gmail.com>
2024-01-09 13:51:26 +1100

    add facts to hero section

---
+++ src/app/components/hero/Fact.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import styles from './hero.module.css';
+import Typography, { TypographyVariant } from '../typography/Typography';
+
+export default function Fact() {
+  return (
+    <div className={styles.factContainer}>
+      <div className={styles.factTitle}>
+        <Typography variant={TypographyVariant.H2} color='var(--GhostWhite);'>
+          Neurodiversity in Adult Education
+        </Typography>
+      </div>
+      <div className={styles.factGrid}>
+        <div className={styles.gridItem} >
+          <Typography variant={TypographyVariant.H1} color='var(--GhostWhite);'>
+            66%
+          </Typography>
+          <Typography
+            variant={TypographyVariant.Body1}
+            color='var(--GhostWhite);'
+          >
+            Of undergraduate courses were less likely than those without
+            disability (69%) to give a positive rating to the quality of their
+            entire educational experience.
+          </Typography>
+        </div>
+        <div className={styles.gridItem}>
+          <Typography variant={TypographyVariant.H1} color='var(--GhostWhite);'>
+            27%
+          </Typography>
+          <Typography
+            variant={TypographyVariant.Body1}
+            color='var(--GhostWhite);;'
+          >
+            Of undergraduate courses were more likely than those without
+            disability (19%) to consider early departure from their course.
+          </Typography>
+        </div>
+        <div className={styles.gridItem}>
+          <Typography variant={TypographyVariant.H1} color='var(--GhostWhite);'>
+            56%
+          </Typography>
+          <Typography
+            variant={TypographyVariant.Body1}
+            color='var(--GhostWhite);'
+          >
+            (56% or 96,400) are less likely to attend a private training
+            provider than those without disability (73% or 2.4 million)
+          </Typography>
+        </div>
+      </div>
+      <div className={styles.factReference}>
+        <Typography
+          variant={TypographyVariant.Body3MOB}
+          color='var(--GhostWhite);'
+        >
+          *People with Disability in Australia 2022
+        </Typography>
+      </div>
+    </div>
+  );
+}
--- src/app/components/hero/Hero.tsx
+++ src/app/components/hero/Hero.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from './Banner';
 import Teacher from './Teacher';
+import Fact from './Fact';
 
 export default function Hero() {
   return (
     <div>
       <Banner></Banner>
       <Teacher></Teacher>
+      <Fact></Fact>
     </div>
   );
 }
