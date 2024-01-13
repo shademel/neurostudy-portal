5132da44068881806b2558697f45cf9c7ac40c11
Majid-pkz <majid.computing@gmail.com>
2024-01-13 16:58:45 +1100

    add about page banner

---
+++ src/app/about/page.tsx
@@ -0,0 +1,17 @@
+'use client';
+import React from 'react';
+import styles from './about.module.css';
+import Typography, {
+  TypographyVariant,
+} from '../components/typography/Typography';
+
+export default function Page() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.text}>
+        <Typography variant={TypographyVariant.H1}>About Us</Typography>
+      </div>
+      <div className={styles.subContainer}></div>
+    </div>
+  );
+}
