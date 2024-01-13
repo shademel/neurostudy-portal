8efacbf5aaed9bedb995d2931ce3b43e0b4db970
Pratik Bhumkar <pratik@neurodiverisityacademy.com>
2024-01-13 22:38:23 +1100

    creating textbox component

--- src/app/components/page.tsx
+++ src/app/components/page.tsx
@@ -1,3 +1,4 @@
+'use client';
 import CardList from './article/card';
 import ButtonDisplay from './buttons/ButtonDisplay';
 import DisplayPodcast from './podcast/DisplayPodcast';
@@ -5,8 +6,12 @@ import Example from './typography/Example';
 import styles from '../page.module.css';
 import BadgeDisplay from './badges/BadgeDisplay';
 import Typography, { TypographyVariant } from './typography/Typography';
+import { useState, ChangeEvent } from 'react';
+import Textbox, { TextboxVariant } from './textbox/Textbox';
 
 export default function Components() {
+  const [name, setName] = useState('');
+
   return (
     <main className={styles.main}>
       <Typography variant={TypographyVariant.H1}>Components</Typography>
@@ -25,7 +30,7 @@ export default function Components() {
         <br />
         <Example />
       </div>
-      <div className={styles.border}>
+      <div>
         <Typography variant={TypographyVariant.H2}>
           Cards/Articles/Blog posts
         </Typography>
@@ -34,6 +39,63 @@ export default function Components() {
       <div className={styles.border}>
         <Typography variant={TypographyVariant.H2}>Podcast embed</Typography>
         <DisplayPodcast />
+      </div>
+      <div className={styles.border}>
+        <Typography variant={TypographyVariant.H2}>Textboxes</Typography>
+        <Typography variant={TypographyVariant.Body2}>Regular</Typography>
+        <Textbox
+          name={'Username'}
+          label={'Username'}
+          type={'text'}
+          value={name}
+          required={false}
+          placeholder={'Name goes here'}
+          // errorMessage={'Error'}
+          variant={TextboxVariant.REGULAR}
+          onChange={function (event: ChangeEvent<HTMLInputElement>): void {
+            setName(event.target.value);
+          }}
+        />
+        <Typography variant={TypographyVariant.Body2}>Long</Typography>
+        <Textbox
+          name={'Username'}
+          label={'Username'}
+          type={'text'}
+          value={name}
+          required={false}
+          placeholder={'Name goes here'}
+          variant={TextboxVariant.LONG}
+          onChange={function (event: ChangeEvent<HTMLInputElement>): void {
+            setName(event.target.value);
+          }}
+        />
+        <Typography variant={TypographyVariant.Body2}>Longer</Typography>
+        <Textbox
+          name={'Username'}
+          label={'Username'}
+          type={'text'}
+          value={name}
+          required={false}
+          placeholder={'Name goes here'}
+          variant={TextboxVariant.LONGER}
+          onChange={function (event: ChangeEvent<HTMLInputElement>): void {
+            setName(event.target.value);
+          }}
+        />
+        <Typography variant={TypographyVariant.Body2}>Longer Error</Typography>
+        <Textbox
+          name={'Username'}
+          label={'Username'}
+          type={'text'}
+          value={name}
+          required={false}
+          placeholder={'Name goes here'}
+          errorMessage={'Error'}
+          variant={TextboxVariant.LONGER}
+          onChange={function (event: ChangeEvent<HTMLInputElement>): void {
+            setName(event.target.value);
+          }}
+        />
       </div>
     </main>
   );
---
+++ src/app/components/textbox/Textbox.tsx
@@ -0,0 +1,59 @@
+import { ChangeEvent } from 'react';
+import styles from './textbox.module.css';
+import Typography, { TypographyVariant } from '../typography/Typography';
+
+export enum TextboxVariant {
+  LONG = 'long',
+  LONGER = 'longer',
+  REGULAR = 'regular',
+}
+
+type TextBoxProps = {
+  name: string;
+  label: string;
+  type: string;
+  value: string;
+  required: boolean;
+  placeholder: string;
+  errorMessage?: string;
+  className?: string;
+  variant?: TextboxVariant;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+};
+
+export default function TextBox(props: TextBoxProps) {
+  let textBoxClass = props.className ? props.className : styles.textBox;
+  if (props.errorMessage) {
+    textBoxClass = `${textBoxClass} ${styles.textboxError}`;
+  }
+  if (props.variant === TextboxVariant.LONG) {
+    textBoxClass = `${textBoxClass} ${styles.long}`;
+  } else if (props.variant === TextboxVariant.LONGER) {
+    textBoxClass = `${textBoxClass} ${styles.longer}`;
+  }
+
+  return (
+    <div className={styles.textBoxContainer}>
+      <label htmlFor={props.name}>
+        <Typography
+          variant={TypographyVariant.Body2}
+          color={props.errorMessage && 'red'}
+        >
+          {props.label}
+        </Typography>
+      </label>
+      <input
+        name={props.name}
+        type={props.type}
+        value={props.value}
+        onChange={props.onChange}
+        required={props.required}
+        placeholder={props.placeholder}
+        className={textBoxClass}
+      />
+      {props.errorMessage && (
+        <span className={styles.errorMessage}>{props.errorMessage}</span>
+      )}
+    </div>
+  );
+}
