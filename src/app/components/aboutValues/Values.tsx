617d82f535465ae6e164bee892e1f2d099ea5afc
Majid-pkz <majid.computing@gmail.com>
2024-01-14 23:01:32 +1100

    add more value cards and scrollbar

--- src/app/components/aboutValues/Values.tsx
+++ src/app/components/aboutValues/Values.tsx
@@ -7,6 +7,9 @@ import styles from './values.module.css';
 import support from '../../images/valuesSupport.svg';
 import respect from '../../images/valuesRespect.svg';
 import safety from '../../images/valuesSafety.svg';
+import innovation from '../../images/valueInnovation.svg';
+import fun from '../../images/valueFun.svg';
+import leadership from '../../images/valueLeadership.svg';
 
 export default function Values() {
   return (
@@ -84,6 +87,79 @@ export default function Values() {
               >
                 Everyone should feel safe in the environment they are and the
                 people they interact with.
+              </Typography>
+            </div>
+          </div>
+        </div>
+        <div className={styles.gridItem}>
+          <div>
+            <Image src={innovation} alt={'innovation'}></Image>
+          </div>
+          <div className={styles.gridTextBox}>
+            <div>
+              <Typography
+                variant={TypographyVariant.Body1}
+                color='var( --GhostWhiteVariant)'
+              >
+                Innovation
+              </Typography>
+            </div>
+            <div className={styles.gridText}>
+              <Typography
+                variant={TypographyVariant.Body2}
+                color='var( --GhostWhiteVariant)'
+              >
+                {' '}
+                Innovation and standing out while supporting the Neurodiversity
+                community
+              </Typography>
+            </div>
+          </div>
+        </div>
+        <div className={styles.gridItem}>
+          <div>
+            <Image src={fun} alt={'fun'}></Image>
+          </div>
+          <div className={styles.gridTextBox}>
+            <div>
+              <Typography
+                variant={TypographyVariant.Body1}
+                color='var( --GhostWhiteVariant)'
+              >
+                Fun
+              </Typography>
+            </div>
+            <div className={styles.gridText}>
+              <Typography
+                variant={TypographyVariant.Body2}
+                color='var( --GhostWhiteVariant)'
+              >
+                Everything we do should be all about having fun and working
+                together as a team.
+              </Typography>
+            </div>
+          </div>
+        </div>
+        <div className={styles.gridItem}>
+          <div>
+            <Image src={leadership} alt={'leadership'}></Image>
+          </div>
+          <div className={styles.gridTextBox}>
+            <div>
+              <Typography
+                variant={TypographyVariant.Body1}
+                color='var( --GhostWhiteVariant)'
+              >
+                Leadership
+              </Typography>
+            </div>
+            <div className={styles.gridText}>
+              <Typography
+                variant={TypographyVariant.Body2}
+                color='var( --GhostWhiteVariant)'
+              >
+                We embody leadership in all we do, while actively fostering
+                tomorrow&apos;s leaders
               </Typography>
             </div>
           </div>
