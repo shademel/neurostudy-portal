03c90034130e1b3067abeab4652689e453c1f534
Pratik Bhumkar <pratik@neurodiverisityacademy.com>
2024-01-17 23:16:13 +1100

    adding endpoint for creating teacher registeration

---
+++ src/app/utilities/registerCRMContact.ts
@@ -0,0 +1,34 @@
+import { AxiosResponse } from 'axios';
+import TeacherCRMContactInterface from '../interfaces/TeacherCRMContactInterface';
+import CRMCreateResponseInterface from '../interfaces/CRMCreateResponseInterface';
+import axios from 'axios';
+
+export const registerTeacherContact = async (
+  contact: TeacherCRMContactInterface
+): Promise<CRMCreateResponseInterface | boolean> => {
+  const data = JSON.stringify({
+    properties: contact,
+  });
+
+  const config = {
+    method: 'post',
+    url: `${process.env.CRM_BASE_URL}/objects/contacts`,
+    headers: {
+      authorization: `Bearer ${process.env.CRM_ACCESS_KEY}`,
+      'content-type': 'application/json',
+    },
+    data: data,
+  };
+
+  return await axios
+    .request(config)
+    .then((response: AxiosResponse) => {
+      const createResponse: CRMCreateResponseInterface = response.data;
+      return createResponse;
+    })
+
+    .catch((error: Error) => {
+      console.log(error);
+      return false;
+    });
+};
