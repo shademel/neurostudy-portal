f9a23fab8dfea62a1cc02a3d3b71fa1e9e64e0f0
Majid-pkz <majid.computing@gmail.com>
2024-01-18 13:40:50 +1100

    add popUp for Access courses -unluck content

--- src/app/components/popupSignup/PopUp.tsx
+++ src/app/components/popupSignup/PopUp.tsx
@@ -1,31 +1,77 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Typography, { TypographyVariant } from '../typography/Typography';
 import styles from './popup.module.css';
 import TextBox, { TextboxVariant } from '../textbox/Textbox';
 import ActionButton from '../buttons/ActionButton';
 
-interface PopUpProps {}
+const PopUp: React.FC<{ onClose: () => void }> = ({ onClose }) => {
+  const [emailTextboxVariant, setEmailTextboxVariant] = useState(
+    TextboxVariant.LONGER
+  );
 
-const PopUp: React.FC<PopUpProps> = () => {
+  useEffect(() => {
+    const handleResize = () => {
+      // Change the variant based on the screen width
+      setEmailTextboxVariant(
+        window.innerWidth <= 800
+          ? TextboxVariant.REGULAR
+          : TextboxVariant.LONGER
+      );
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+  }, []);
   const handleSubmit = () => {
-    // router.push(`/????`);
-    console.log('form submited successfully');
+    // Handle form submission logic here
+    console.log('Thank you for your request, we will get back to you soon');
+    // Close the modal after submission
+    onClose();
   };
 
   return (
     <form className={styles.container}>
       <div className={styles.header}>
-        <Typography variant={TypographyVariant.H2}>Unlock Content</Typography>
+        <Typography variant={TypographyVariant.H1}>Unlock Content</Typography>
       </div>
-      <div className={styles.name}>
+      <div className={styles.textboxBody}>
+        <div className={styles.name}>
+          <div className={styles.textArea}>
+            <TextBox
+              name={'firstName'}
+              label={'First Name (required)'}
+              type={'text'}
+              value={''}
+              required={true}
+              placeholder={'example: John'}
+              onChange={() => {
+                console.log('modal');
+              }}
+            ></TextBox>
+          </div>
+          <div className={styles.textArea}>
+            <TextBox
+              name={'lastName'}
+              label={'Last Name (required)'}
+              type={'text'}
+              value={''}
+              required={true}
+              placeholder={'example: Smith'}
+              onChange={() => {
+                console.log('modal');
+              }}
+            ></TextBox>
+          </div>
+        </div>
         <div className={styles.textArea}>
           <TextBox
-            name={'firstName'}
-            label={'First Name* (required)'}
+            name={'phoneNumber'}
+            label={'Phone Number'}
             type={'text'}
             value={''}
-            required={true}
-            placeholder={'example: John'}
+            required={false}
+            placeholder={''}
             onChange={() => {
               console.log('modal');
             }}
@@ -33,52 +79,26 @@ const PopUp: React.FC<PopUpProps> = () => {
         </div>
         <div className={styles.textArea}>
           <TextBox
-            name={'lastName'}
-            label={'Last Name* (required)'}
-            type={'text'}
+            variant={emailTextboxVariant}
+            name={'email'}
+            label={'Email Address (required)'}
+            type={'email'}
             value={''}
             required={true}
-            placeholder={'example: Smith'}
+            placeholder={''}
             onChange={() => {
               console.log('modal');
             }}
           ></TextBox>
         </div>
-      </div>
-      <div className={styles.textArea}>
-        <TextBox
-          name={'phoneNumber'}
-          label={'Phone Number'}
-          type={'text'}
-          value={''}
-          required={false}
-          placeholder={''}
-          onChange={() => {
-            console.log('modal');
-          }}
-        ></TextBox>
-      </div>
-      <div className={styles.textArea}>
-        <TextBox
-          variant={TextboxVariant.LONGER}
-          name={'email'}
-          label={'Email Address* (required)'}
-          type={'email'}
-          value={''}
-          required={true}
-          placeholder={''}
-          onChange={() => {
-            console.log('modal');
-          }}
-        ></TextBox>
-      </div>
-      <div className={styles.buttonArea}>
-        <ActionButton
-          label='Submit'
-          disabled={false}
-          className={styles.accessButton}
-          onClick={() => handleSubmit()}
-        />
+        <div className={styles.buttonArea}>
+          <ActionButton
+            label='Submit'
+            disabled={false}
+            className={styles.accessButton}
+            onClick={() => handleSubmit()}
+          />
+        </div>
       </div>
     </form>
   );
