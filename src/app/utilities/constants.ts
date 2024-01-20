02e06466dfd2e164feda793330aff2d07905c74f
Majid-pkz <majid.computing@gmail.com>
2024-01-20 16:58:31 +1100

    add new Phone regex to include landline, rename the old one to Mobile

--- src/app/utilities/constants.ts
+++ src/app/utilities/constants.ts
@@ -8,6 +8,7 @@ export enum COMPANY {
 export const EMAIL_REGEX =
   /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}(\.[a-zA-Z]{2,})?$/;
 
-export const PHONE_REGEX = /^(?:\+61|0)[2-478](?:\d{8})$/;
+export const MOBILE_REGEX = /^(?:\+61|0)[2-478](?:\d{8})$/;
+export const PHONE_REGEX = /^(?:\+61|0)?[2-9]\d{8}$/;
 
 export const NAME_REGEX = /^[a-zA-Z0-9. -]+$/;
