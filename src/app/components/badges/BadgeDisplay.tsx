0f5d44b3621c1f6a9b85dd4cadea70d76556b5d8
Majid-pkz <majid.computing@gmail.com>
2024-01-22 12:59:12 +1100

    add badges to hero section banner

--- src/app/components/badges/BadgeDisplay.tsx
+++ src/app/components/badges/BadgeDisplay.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import Image from 'next/image';
 import styles from './badgeDisplay.module.css';
-import bg from '../../images/bg.svg';
 import goldBadge from '../../images/goldBadge.svg';
+import silverBadge from '../../images/silverBadge.svg';
+import bronzeBadge from '../../images/bronzeBadge.svg';
 
 export default function BadgeDisplay() {
   return (
-    <div className={styles.badgeContainer}>
-      <Image src={bg} alt='Background' />
-
-      <div className={styles.badgeOverlay}>
+    <div className={styles.badgeWrapper}>
+      <div className={styles.gold}>
         <Image src={goldBadge} alt='Gold Badge' />
+      </div>
+      <div className={styles.silver}>
+        <Image src={silverBadge} alt='Gold Badge' />
+        <Image src={bronzeBadge} alt='Gold Badge' />
       </div>
     </div>
   );
