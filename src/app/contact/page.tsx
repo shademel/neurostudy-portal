2f0c4341811a701282ca3f30b6d54b5d04ce3a8e
Pratik Bhumkar <150103212+bhumkarpratik@users.noreply.github.com>
2024-05-25 08:39:49 +1000

    Add Metadata to contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -2,6 +2,13 @@
 import React from 'react';
 import styles from './contact.module.css';
 import ContactUsForm from '../components/contactUs/ContactUsForm';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Contact',
+  description: 'Contact page allows customers to contact Neurodiversity Academy.',
+  keywords: ['contact', 'neurodiversity']
+};
 
 export default function Page() {
   return (
