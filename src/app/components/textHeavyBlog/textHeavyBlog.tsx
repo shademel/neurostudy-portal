4852dda04f46bcf526b14b21f6648c1f9d2515e1
mikemike27 <mikeymike3193@gmail.com>
2024-06-03 17:23:00 +1000

    Updated branch
    1. updated branch to latest changes

--- src/app/components/textHeavyBlog/textHeavyBlog.tsx
+++ src/app/components/textHeavyBlog/textHeavyBlog.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import styles from './textHeavyBlog.module.css';
 import DOMPurify from 'dompurify';
+import BlogList from '../blogList/blogList';
 
 export default function TextHeavyBlog({
   header,
