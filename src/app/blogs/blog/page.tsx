746c6082e4cbffb39c29b7d38cce9ad0afdd0c6b
Salman Hossain <salmanhossain_007@yahoo.com>
2024-06-10 00:46:24 +1000

    Blog - integrating metadata properly

--- src/app/blogs/blog/page.tsx
+++ src/app/blogs/blog/page.tsx
@@ -1,58 +1,70 @@
-'use client';
 import React from 'react';
 import styles from './blog.module.css';
 import blogData from '../blogData.json';
-import { useSearchParams } from 'next/navigation';
 import Typography, {
   TypographyVariant,
 } from '../../components/typography/Typography';
 import TextHeavyBlog from '../../components/textHeavyBlog/textHeavyBlog';
 import BlogList from '@/app/components/blogList/blogList';
 import DisplayPodcast from '@/app/components/podcast/DisplayPodcast';
+import { MetadataProps } from '@/app/interfaces/MetadataProps';
+import { Metadata } from 'next';
 
-export default function OneBlog() {
-  const searchParams = useSearchParams();
+export async function generateMetadata({
+  searchParams,
+}: MetadataProps): Promise<Metadata> {
+  const blogId = searchParams.blogId;
   const { blogs } = blogData;
-  const blogId = searchParams.get('blogId');
-  const blog = blogs.find((blog) => blog.id === blogId);
-  if (blog) {
-    return (
-      <>
-        <head>
-          <title>{blog.header}</title>
-          <meta
-            name='keywords'
-            content='blog, neurodiversity, neurodiversity academy, neurodivergent mates, neurodivergentmates'
-          ></meta>
-          <meta property='og:title' content={blog.header} />
-          <meta property='og:image' content={blog.imageUrl} />
-          <meta property='og:type' content='blog' />
-          <meta property='og:description' content={blog?.description} />
-          <meta
-            property='og:url'
-            content={`https://neurodiversityacademy.com/blogs/blog?blogId=${blogId}`}
-          />
-          <meta name='description' content={blog?.description}></meta>
-        </head>
-        <div className={styles.container}>
-          <TextHeavyBlog
-            id={blog.id}
-            header={blog.header}
-            imageUrl={blog.imageUrl}
-            bodyText={blog.bodyText}
-          />{' '}
-          <DisplayPodcast
-            scriptSrc={blog.scriptSrc}
-            containerId={blog.containerId}
-            singleBlog={true}
-          />
-          <BlogList />
-        </div>
-      </>
-    );
-  } else {
+  const blog = blogs.find(({ id }) => id === blogId);
+
+  if (!blog) {
+    return {};
+  }
+
+  const { title, imageUrl, description } = blog;
+  // NOTE
+  // Previous `type` was 'blog', which the `openGraph` attribute doesn't accept as `type`
+  const type = 'article';
+  const url = `https://neurodiversityacademy.com/blogs/blog?blogId=${blogId}`;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      images: [imageUrl],
+      type,
+      description,
+      url,
+    },
+  };
+}
+
+export default function OneBlog({ searchParams }: MetadataProps) {
+  const blogId = searchParams.blogId;
+  const { blogs } = blogData;
+  const blog = blogs.find(({ id }) => id === blogId);
+
+  if (!blog) {
     return (
       <Typography variant={TypographyVariant.H1}>Blog not found</Typography>
     );
   }
+
+  return (
+    <div className={styles.container}>
+      <TextHeavyBlog
+        id={blog.id}
+        header={blog.header}
+        imageUrl={blog.imageUrl}
+        bodyText={blog.bodyText}
+      />{' '}
+      <DisplayPodcast
+        scriptSrc={blog.scriptSrc}
+        containerId={blog.containerId}
+        singleBlog={true}
+      />
+      <BlogList />
+    </div>
+  );
 }
