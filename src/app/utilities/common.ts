5825dc1fff42ce44db01ef45b7c7400019fa9030
Salman Hossain <salmanhossain_007@yahoo.com>
2024-06-09 23:33:09 +1000

    Throttle & Debounce - enhancing type specification

--- src/app/utilities/common.ts
+++ src/app/utilities/common.ts
@@ -1,28 +1,39 @@
-export const debounce = (fn: Function, threshold: number = 500, context: any = null) => {
-  let timer: any
-  return function (...args: any[]) {
-    clearTimeout(timer)
+type RegulatorPropFn = (...args: unknown[]) => unknown;
+
+export const debounce = (
+  fn: RegulatorPropFn,
+  threshold: number = 500,
+  context: object | null = null
+) => {
+  let timer: ReturnType<typeof setTimeout>;
+  return function (...args: unknown[]) {
+    clearTimeout(timer);
     timer = setTimeout(function () {
-      fn.apply(context, args)
-    }, threshold)
-  }
-}
+      fn.apply(context, args);
+    }, threshold);
+  };
+};
 
-export const throttle = (fn: Function, threshold: number = 500, context: any = null) => {
-  let last: number = -1, timer: any
+export const throttle = (
+  fn: RegulatorPropFn,
+  threshold: number = 500,
+  context: object | null = null
+) => {
+  let last: number = -1,
+    timer: ReturnType<typeof setTimeout>;
 
-  return function (...args: any[]) {
-    let now = Date.now()
+  return function (...args: unknown[]) {
+    const now = Date.now();
 
     if (last && now < last + threshold) {
-      clearTimeout(timer)
+      clearTimeout(timer);
       timer = setTimeout(function () {
-        last = now
-        fn.apply(context, args)
-      }, threshold)
+        last = now;
+        fn.apply(context, args);
+      }, threshold);
     } else {
-      last = now
-      fn.apply(context, args)
+      last = now;
+      fn.apply(context, args);
     }
-  }
-}
+  };
+};
