a0d84ed368e0dc613240534ffffc5d57f2c68278
Salman Hossain <salmanhossain_007@yahoo.com>
2024-06-09 21:33:58 +1000

    Integrating useWindowWidth() with components

--- src/app/components/textHeavyArticle/TextHeavyArticle.tsx
+++ src/app/components/textHeavyArticle/TextHeavyArticle.tsx
@@ -1,22 +1,19 @@
 'use client';
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { TextHeavyInterface } from '@/app/interfaces/TextHeavyInterface';
 import Typography, { TypographyVariant } from '../typography/Typography';
 import Image from 'next/image';
 import Link from 'next/link';
 import styles from './textHeavyArticle.module.css';
 import DOMPurify from 'dompurify';
+import useWindowWidth from '@/app/hooks/useWindowWidth';
 
 export default function TextHeavyArticle({
   header,
   imageUrl,
   bodyText,
 }: TextHeavyInterface): JSX.Element {
-  const [windowWidth, setWindowWidth] = useState(1150);
-
-  useEffect(() => {
-    setWindowWidth(window.innerWidth);
-  });
+  const windowWidth = useWindowWidth()
 
   const paragraphs = bodyText.split('\n').map((paragraph, index) => {
     const sanitizedHTML = DOMPurify.sanitize?.(paragraph);
