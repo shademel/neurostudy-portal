1d07ceea29c61e513c117807b83b33d5e7a591e0
Salman Hossain <salmanhossain_007@yahoo.com>
2024-06-11 18:39:38 +1000

    ContactUsForm & Teacher - adding `use client`

--- src/app/components/teacherSection/Teacher.tsx
+++ src/app/components/teacherSection/Teacher.tsx
@@ -1,3 +1,4 @@
+'use client';
 import React, { useCallback, useState } from 'react';
 import styles from './teacher.module.css';
 import Typography, { TypographyVariant } from '../typography/Typography';
