c0d6c876199c6aa88dcbe8c87d1ea349c47b9c18
mikemike27 <mikeymike3193@gmail.com>
2024-06-12 19:35:10 +1000

    Revert "Merge branch 'created-subscription-section' of https://github.com/NeurodiversityAcademy/neurostudy-portal into created-subscription-section"
    This reverts commit d6b358c71ebfc4d4d14c0f8dc9e0efdec85b95dc, reversing
    changes made to 489fe64344933488314458be0a10dcbbcc51dfd7.

--- src/app/components/buttons/MyLogin.tsx
+++ src/app/components/buttons/MyLogin.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import React from 'react';
 import ArrowDownCircle from '../../images/arrowDownCircle.svg';
 import ActionButton from './ActionButton';
--- src/app/components/navbar/Navbar.tsx
+++ src/app/components/navbar/Navbar.tsx
@@ -2,7 +2,7 @@
 import styles from './navbar.module.css';
 import { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
-import MyLogin from '../buttons/MyLogin';
+// import MyLogin from '../buttons/MyLogin';
 import Image from 'next/image';
 import Logo from '../../images/Logo-navbar.svg';
 import Hamburger from '../../images/hamburgerMenu.svg';
@@ -112,7 +112,7 @@ export default function Navbar() {
               </Link>
             </li>
           </ul>
-          <MyLogin className={styles.login} />
+          {/* <MyLogin className={styles.login} /> */}
         </div>
         <Image
           className={styles.hamburger}
