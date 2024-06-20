1fae2a27f8363e857f470c56f1e6e004171fb1ca
Salman Hossain <salmanhossain_007@yahoo.com>
2024-06-21 00:27:51 +1000

    Signup - utilizing `foundations` css

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -3,20 +3,24 @@
 import styles from '../page.module.css';
 import AuthLeftBanner from '../component/AuthLeftBanner';
 import AuthFormFooter from '../component/AuthFormFooter';
+import classNames from 'classnames';
 
 const Page = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.container}>
+    <main className='container'>
+      <div className='row'>
         <AuthLeftBanner />
-        <div className={styles.textColumn}>
+        <div className={classNames(styles.textColumn, 'col-md-8')}>
           <div className={styles.textColumnWrapper}>
             <div className={styles.textTitleWrapper}>
               <p className={styles.textTitle}>Sign Up</p>
               <span className={styles.smallTextTitle}>Its quick and easy</span>
             </div>
             <form className={styles.form}>
-
+              <div className='row'>
+                <div className='col-sm-5'>Hello</div>
+                <div className='col-sm-7'>World</div>
+              </div>
             </form>
             <AuthFormFooter />
           </div>
