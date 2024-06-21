947bae8cf13c07e599549bbfde9d44f09762d0ca
Salman Hossain <salmanhossain_007@yahoo.com>
2024-06-21 17:30:00 +1000

    Finishing Sign Up form interface only

--- src/app/auth/signup/page.tsx
+++ src/app/auth/signup/page.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import styles from '../page.module.css';
+import commonStyles from '@/app/styles/common.module.css';
+import ActionButton from '@/app/components/buttons/ActionButton';
 import AuthLeftBanner from '../component/AuthLeftBanner';
 import AuthFormFooter from '../component/AuthFormFooter';
 import classNames from 'classnames';
@@ -10,6 +12,10 @@ import TextBox, {
 } from '@/app/components/formElements/TextBox';
 import Form from '@/app/components/formElements/Form';
 import { EMAIL_REGEX } from '@/app/utilities/constants';
+import Typography, {
+  TypographyVariant,
+} from '@/app/components/typography/Typography';
+import Link from 'next/link';
 
 interface SignUpFieldValues extends FieldValues {
   firstName: string;
@@ -58,6 +64,60 @@ const SignUpPage = () => {
                 placeholder='Email address'
                 pattern={EMAIL_REGEX}
               />
+              <TextBox
+                name='password'
+                type='password'
+                label='New Password'
+                required
+                placeholder='New Password'
+                autoComplete='new-password'
+              />
+              <Typography
+                variant={TypographyVariant.Body3MOB}
+                className={classNames(styles.dobLabel, 'col-md-12', 'pt-2')}
+              >
+                Date of Birth
+              </Typography>
+              <div className='col-md-12'>
+                <div className={classNames(styles.dobInputContainer, 'row')}>
+                  <TextBox
+                    name='date'
+                    type='number'
+                    label='Date'
+                    required
+                    placeholder='Date'
+                  />
+                  <TextBox
+                    name='month'
+                    type='number'
+                    label='Month'
+                    required
+                    placeholder='Month'
+                  />
+                  <TextBox
+                    name='year'
+                    type='number'
+                    label='Year'
+                    required
+                    placeholder='Year'
+                  />
+                </div>
+              </div>
+              <Typography
+                variant={TypographyVariant.Body2}
+                className={classNames('pt-3 pb-2', commonStyles.textCenter)}
+              >
+                By signing up, you agree to our{' '}
+                <Link href='#'>Terms and Conditions</Link>
+              </Typography>
+              <div>
+                <ActionButton
+                  type='submit'
+                  label='Sign Up'
+                  disabled={true}
+                  className={styles.accessButton}
+                />
+              </div>
             </Form>
             <AuthFormFooter />
           </div>
