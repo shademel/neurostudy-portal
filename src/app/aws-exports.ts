65e199f7aa013fcf3fbef30de276a1c12aac765a
Salman Hossain <salmanhossain_007@yahoo.com>
2024-06-26 16:01:22 +1000

    Updating process.env aws keys

--- src/app/aws-exports.ts
+++ src/app/aws-exports.ts
@@ -1,17 +1,17 @@
 import { ResourcesConfig } from 'aws-amplify';
 
-const NEXT_PUBLIC_COGNITO_USERPOOL_ID =
-  process.env.NEXT_PUBLIC_COGNITO_USERPOOL_ID;
+const NEXT_PUBLIC_COGNITO_USER_POOL_ID =
+  process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID;
 const NEXT_PUBLIC_COGNITO_CLIENT_ID = process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID;
 
-if (!NEXT_PUBLIC_COGNITO_USERPOOL_ID || !NEXT_PUBLIC_COGNITO_CLIENT_ID) {
+if (!NEXT_PUBLIC_COGNITO_USER_POOL_ID || !NEXT_PUBLIC_COGNITO_CLIENT_ID) {
   throw new Error('Cognito variables have not been properly initialized.');
 }
 
 const awsconfig: ResourcesConfig = {
   Auth: {
     Cognito: {
-      userPoolId: NEXT_PUBLIC_COGNITO_USERPOOL_ID,
+      userPoolId: NEXT_PUBLIC_COGNITO_USER_POOL_ID,
       userPoolClientId: NEXT_PUBLIC_COGNITO_CLIENT_ID,
     },
   },
