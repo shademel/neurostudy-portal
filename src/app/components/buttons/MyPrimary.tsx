8f364bb8a23bfe2f51c4d215ee7b078d0e144d32
mikemike27 <mikeymike3193@gmail.com>
2024-07-11 23:58:59 +1000

    Update ButtonDisplay component
    1. update ButtonDisplay component to include the new changes of button component

--- src/app/components/buttons/MyPrimary.tsx
+++ src/app/components/buttons/MyPrimary.tsx
@@ -3,14 +3,42 @@
 import React from 'react';
 import Search from '../../images/Search.svg';
 import ActionButton from './ActionButton';
+import { BUTTON_STYLE } from '@/app/utilities/constants';
 
 export default function myPrimary() {
   return (
-    <ActionButton
-      label='Search'
-      icon={Search}
-      disabled={false}
-      onClick={() => console.log('clicked Primary button')}
-    />
+    <div className='row'>
+      <ActionButton
+        label='Search'
+        icon={Search}
+        onClick={() => console.log('clicked Primary button')}
+        style={BUTTON_STYLE.Primary}
+        className='mb-3'
+      />
+      <ActionButton
+        label='Search'
+        icon={Search}
+        onClick={() => console.log('clicked Primary button')}
+        style={BUTTON_STYLE.Primary}
+        fullWidth
+        className='mb-3'
+      />
+      <ActionButton
+        label='Search'
+        icon={Search}
+        onClick={() => console.log('clicked Primary button')}
+        style={BUTTON_STYLE.Primary}
+        className='mb-3'
+        disabled
+      />
+      <ActionButton
+        label='Search'
+        icon={Search}
+        onClick={() => console.log('clicked Primary button')}
+        style={BUTTON_STYLE.Primary}
+        fullWidth
+        disabled
+      />
+    </div>
   );
 }
--- src/app/components/buttons/MyTertiary.tsx
+++ src/app/components/buttons/MyTertiary.tsx
@@ -5,13 +5,42 @@ import { BUTTON_STYLE } from '@/app/utilities/constants';
 
 export default function myTertiary() {
   return (
-    <ActionButton
-      label='Learn more'
-      icon={CircleRight}
-      style={BUTTON_STYLE.Tertiary}
-      disabled={false}
-      iconPosition='right'
-      onClick={() => console.log('clicked Tertiary button')}
-    />
+    <div className='row'>
+      <ActionButton
+        label='Learn more'
+        icon={CircleRight}
+        style={BUTTON_STYLE.Tertiary}
+        iconPosition='right'
+        onClick={() => console.log('clicked Tertiary button')}
+        className='mb-3'
+      />
+      <ActionButton
+        label='Learn more'
+        icon={CircleRight}
+        style={BUTTON_STYLE.Tertiary}
+        iconPosition='right'
+        onClick={() => console.log('clicked Tertiary button')}
+        fullWidth
+        className='mb-3'
+      />
+      <ActionButton
+        label='Learn more'
+        icon={CircleRight}
+        style={BUTTON_STYLE.Tertiary}
+        iconPosition='right'
+        onClick={() => console.log('clicked Tertiary button')}
+        className='mb-3'
+        disabled
+      />
+      <ActionButton
+        label='Learn more'
+        icon={CircleRight}
+        style={BUTTON_STYLE.Tertiary}
+        iconPosition='right'
+        onClick={() => console.log('clicked Tertiary button')}
+        fullWidth
+        disabled
+      />
+    </div>
   );
 }
