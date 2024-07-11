9386db185c760dbe72dad97f5da8312e8d252507
mikemike27 <mikeymike3193@gmail.com>
2024-07-11 23:55:36 +1000

    Update ButtonDisplay component
    1. update ButtonDisplay component to include the new changes of button component

--- src/app/components/buttons/MySecondary.tsx
+++ src/app/components/buttons/MySecondary.tsx
@@ -5,12 +5,38 @@ import { BUTTON_STYLE } from '@/app/utilities/constants';
 
 export default function mySecondary() {
   return (
-    <ActionButton
-      label='Search'
-      icon={Search}
-      style={BUTTON_STYLE.Secondary}
-      disabled={false}
-      onClick={() => console.log('clicked Secondary button')}
-    />
+    <div className='row'>
+      <ActionButton
+        label='Search'
+        icon={Search}
+        onClick={() => console.log('clicked Secondary button')}
+        style={BUTTON_STYLE.Secondary}
+        className='mb-3'
+      />
+      <ActionButton
+        label='Search'
+        icon={Search}
+        onClick={() => console.log('clicked Secondary button')}
+        style={BUTTON_STYLE.Secondary}
+        fullWidth
+        className='mb-3'
+      />
+      <ActionButton
+        label='Search'
+        icon={Search}
+        onClick={() => console.log('clicked Secondary button')}
+        style={BUTTON_STYLE.Secondary}
+        className='mb-3'
+        disabled
+      />
+      <ActionButton
+        label='Search'
+        icon={Search}
+        onClick={() => console.log('clicked Secondary button')}
+        style={BUTTON_STYLE.Secondary}
+        fullWidth
+        disabled
+      />
+    </div>
   );
 }
