4598215de5f09ac5267145fd8f9469d1ace45dbb
shademel <127929064+shademel@users.noreply.github.com>
2024-07-18 07:01:56 +0000

    Updated Contact page

--- src/app/components/contactUs/ContactUsLeftBanner.tsx
+++ src/app/components/contactUs/ContactUsLeftBanner.tsx
@@ -7,10 +7,10 @@ import Typography, {
 const ContactUsLeftBanner = () => {
   return (
     <div className={classNames(styles.bannerColumn, 'col-md-4')}>
-      <Typography variant={TypographyVariant.H2} className='mt-0'>
+      <Typography variant={TypographyVariant.H2} color='var(--GhostWhite' className='mt-0 mb-3'>
         Contact Us
       </Typography>
-      <Typography variant={TypographyVariant.Body1}>
+      <Typography variant={TypographyVariant.Body2} color='var(--GhostWhite)'>
         We will try our best to get back to you as soon as possible
       </Typography>
     </div>
